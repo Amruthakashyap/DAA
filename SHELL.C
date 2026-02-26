1b91d370e75e0111aa942d15d97c26173ffd0090
Amrutha <amruthaskashyap4@gmail.com>
2026-02-26 12:01:20 +0530

    Add files via upload

--- SHELL.C
+++ SHELL.C
@@ -1,36 +1,52 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<conio.h>
-#include<stdlib.h>
+#include<time.h>
+
+void shellSort(int arr[], int n)
+{
+    int gap, i, j, temp;
+
+    for (gap = n / 2; gap > 0; gap = gap / 2)
+    {
+	for (i = gap; i < n; i++)
+	{
+	    for (j = i - gap; j >= 0; j = j - gap)
+	    {
+		if (arr[j + gap] >= arr[j])
+		    break;
+		else
+		{
+		    temp = arr[j];
+		    arr[j] = arr[j + gap];
+		    arr[j + gap] = temp;
+		}
+	    }
+	}
+    }
+}
+
 void main()
 {
-int n,j,a[10],key,gap,i,t=0;
-clrscr();
-printf("enter the size");
-scanf("%d",&n);
-printf("enter the elements\n");
-for(i=0;i<n;i++)
-scanf("%d\n",&a[i]);
-for(gap=n/2;gap>0;gap=gap/=2)
-{
-t=t+3;
-for(i=gap;i<n;i++)
-{
-t=t+3;
-key=a[i];
-t=t+2;
-for(j=i;j>=gap && a[j-gap]>key;j=j-gap)
-{
-t=t+3;
-a[j]=a[j-gap];
-t=t+2;
-}
-a[j]=key;
-t=t+1;
-}
-}
-printf("sorted array");
-for(i=0;i<n;i++)
-printf("%d\n",a[i]);
-printf("total time is %d",t);
-getch();
+    int arr[100], n, i;
+    clrscr();
+    srand(time(NULL));
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++)
+    {
+	arr[i]=rand()%100;
+	printf("%d ",arr[i]);
+    }
+
+    shellSort(arr, n);
+
+    printf("\nSorted array:\n");
+    for (i = 0; i < n; i++)
+    {
+	printf("%d ", arr[i]);
+    }
+
+    getch();
 }
